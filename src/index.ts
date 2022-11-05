554be6ed509f0f977ba75262ca28f6fda4b5a0d0
André Rodrigues <andre.rodrigues98@outlook.com>
2022-11-05 15:53:33 -0300

    adding support to typescript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import 'express-async-errors';
+import express, { Request, Response } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+app.get('/home', async (req: Request, res: Response) => {
+    res.json({ hello: 'world' });
+});
+
+app.listen(3000, () => {
+    console.log('running app');
+})
